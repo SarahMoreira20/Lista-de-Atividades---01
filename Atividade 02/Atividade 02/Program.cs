07f6953c3ced66f6a7abfea3a0f8bbe45e6b3829
Sarah Moreira <moreiradavitoriasarah@gmail.com>
2025-09-03 10:43:07 -0300

    Atividade 02 - número antecessor

---
+++ Atividade 02/Atividade 02/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+int valor;
+Console.WriteLine("Digite um valor: ");
+valor = int.Parse(Console.ReadLine());
+Console.WriteLine($"O numero antecessor é: {valor - 1}");
