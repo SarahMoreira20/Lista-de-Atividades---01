5dfc3da4a0ae10e49b9013f37585f24bca0adaf6
Sarah Moreira <moreiradavitoriasarah@gmail.com>
2025-09-10 10:59:41 -0300

    Extra 05 - aumento de salário

---
+++ Extra 05/Extra 05/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escreva o seu salário atual");
+float salario = float.Parse(Console.ReadLine());
+Console.WriteLine($"Seu salário depois de um aumento de 15% será de: R${salario * 1.15}");
