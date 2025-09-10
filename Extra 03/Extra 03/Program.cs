88c8a286d648307fe148406c2a53b572507dd2f4
Sarah Moreira <moreiradavitoriasarah@gmail.com>
2025-09-10 10:57:51 -0300

    Extra 03 - icógnitas

---
+++ Extra 03/Extra 03/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Escreva a icognita A");
+double a = double.Parse(Console.ReadLine());
+Console.WriteLine("Escreva a icognita B");
+double b = double.Parse(Console.ReadLine());
+Console.WriteLine("Escreva a icognita C");
+double c = double.Parse(Console.ReadLine());
+
+Console.WriteLine($"O Valor de delta é de: {((b * b) * (-4 * a * c))}");
