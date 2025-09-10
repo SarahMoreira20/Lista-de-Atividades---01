4faeeb6ced21152771d8cbd511417c819c39f219
Sarah Moreira <moreiradavitoriasarah@gmail.com>
2025-09-10 08:22:00 -0300

    Atividade 08 - valor maior que zero

---
+++ Atividade 08/Atividade 08/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.ComponentModel.Design;
+
+Console.WriteLine("Escreva uma valor que não seja zero");
+int valor = int.Parse(Console.ReadLine());
+
+    if (valor == 0)
+    {
+        Console.WriteLine("Valor inválido.Zero não é permitido!");
+    }
+    else if (valor > 0)
+    {
+        Console.WriteLine("O valor é positivo!");
+    }
+    else
+    {
+    Console.WriteLine("O valor é negativo!");
+    }
