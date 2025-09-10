425d46981fda68b314cff2e4c3a2b367c7ae06af
Sarah Moreira <moreiradavitoriasarah@gmail.com>
2025-09-10 10:52:20 -0300

    Extra 01

---
+++ Extra 01/Extra 01/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite a sua quantia em real");
+double real = double.Parse(Console.ReadLine());
+Console.WriteLine($"A quantia que você pode comprar em dolar é de : {real / 3.45}");
