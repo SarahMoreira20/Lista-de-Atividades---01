aac170480ec40176f2cced01f02e6f2c6fbffaff
Sarah Moreira <moreiradavitoriasarah@gmail.com>
2025-09-10 08:46:44 -0300

    Atividade 09 - num maçãs

---
+++ Atividade 09/Atividade 09/Program.cs
@@ -0,0 +1,18 @@
+﻿
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite o número de maçãs que você comprou: ");
+int numMacas = int.Parse(Console.ReadLine());
+double preco;
+
+if (numMacas < 12)
+{
+    preco = 1.30;
+}
+else
+{
+    preco = 1.00;
+}
+
+double total = numMacas * preco;
+
+Console.WriteLine($"O valor da compra é de R$ {total}");
