4785d1afe9286d9bfcaf82729a99f75fb8f1c788
Sarah Moreira <moreiradavitoriasarah@gmail.com>
2025-09-10 10:58:53 -0300

    Extra 04 - produto com desconto

---
+++ Extra 04/Extra 04/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Me diga o preço do produto");
+double preco = double.Parse(Console.ReadLine());
+Console.WriteLine($"O preço do produto com desconto será: R${preco * 0.95}");
+
