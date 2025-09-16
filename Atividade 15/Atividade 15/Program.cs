e9bf2e6ccc666bcd8740e9e37fe89f849efc614f
Sarah Moreira <moreiradavitoriasarah@gmail.com>
2025-09-16 08:28:22 -0300

    Atividade 15 - Números entre 1000 e 2000 com resto 5 na divisão por 11

---
+++ Atividade 15/Atividade 15/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Números entre 1000 e 2000 que divididos por 11 dão resto 5:\n");
+
+for (int i = 1000; i <= 2000; i++)
+{
+    if (i % 11 == 5)
+    {
+        Console.WriteLine(i);
+    }
+}
