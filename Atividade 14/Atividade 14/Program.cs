8ee52032e5cd00678f22a20b90cd194e5b289755
Sarah Moreira <moreiradavitoriasarah@gmail.com>
2025-09-16 08:22:27 -0300

    Atividade 14 - impressão 1-N

---
+++ Atividade 14/Atividade 14/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("digite um número maior que ZERO");
+int N = int.Parse(Console.ReadLine());
+Console.WriteLine($"\nNúmeros de 1 até {N}:");
+
+for (int i = 1; i <= N; i++)
+{
+    Console.WriteLine(i);
+}
