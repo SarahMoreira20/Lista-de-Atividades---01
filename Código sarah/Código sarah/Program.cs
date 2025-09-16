08b5bb7513cd22a42be1299adaba30311aa6742f
Sarah Moreira <moreiradavitoriasarah@gmail.com>
2025-09-16 07:42:23 -0300

    Código criado por sarah

---
+++ Código sarah/Código sarah/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.Write("Digite sua função no escritório SF: ");
+string funcao = Console.ReadLine().ToLower();
+
+if (funcao == "advogada")
+{
+    Console.WriteLine("Olá Dra. Sara");
+}
+else if (funcao == "estagiário")
+{
+    Console.WriteLine("Olá Sarah / Luiz Felipe");
+}
+else if (funcao == "assistente administrativo")
+{
+    Console.WriteLine("Olá Ynara / Thamyres!");
+}
+else
+{
+    Console.WriteLine("Função não reconhecida.");
+}
